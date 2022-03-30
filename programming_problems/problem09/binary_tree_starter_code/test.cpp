3b3ec3303c14f15dde0b2c62ad240dfd7af9b998
jrkinneer <jrkinneer@gmail.com>
2022-03-30 16:56:37 -0400

    homework 9 BinaryTree

---
+++ programming_problems/problem09/binary_tree_starter_code/test.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using std::cout;
+using std::endl;
+
+#include "BinaryTree.h"
+
+typedef std::string ItemType;
+typedef void (*FunctionType)(ItemType& anItem);
+
+void PrintNode(ItemType& i) { cout << i << endl; };
+
+int main(int argc, char** argv)
+{
+    BinaryTree<ItemType, FunctionType> T1("B");
+    BinaryTree<ItemType, FunctionType> T2("C");
+
+    BinaryTree<ItemType, FunctionType> T3("A");
+    T3.attachLeftSubtree(T1);
+    T3.attachRightSubtree(T2);
+    T3.postorderTraverse(&PrintNode);
+    std::cout<<"\n";
+    T3.preorderTraverse(&PrintNode);
+    std::cout<<"\n";
+    T3.inorderTraverse(&PrintNode);
+
+    // now T1 and T2 are no longer trees, but empty objects
+
+    return 0;
+};
